beda327dbe41a095d6e7d6343e6c97b09867a42e
Amjad Jibon <amjad.jibon@gmail.com>
2022-12-23 18:58:33 +0600

    todoapp colors added

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
 const todoBGColor = Color(0xFFE5E5E5);
+const todoBlack = Color(0xFF000000);
+const todoWhite = Color(0xFFFFFFFF);
+const todoGrey = Color(0xFFBDBDBD);
+const todoRed = Color(0xFFE57373);
+const todoGreen = Color(0xFF81C784);
+const todoBlue = Color(0xFF64B5F6);
+const todoYellow = Color(0xFFFFF176);
+const todoOrange = Color(0xFFFFB74D);
+const todoPurple = Color(0xFFBA68C8);
